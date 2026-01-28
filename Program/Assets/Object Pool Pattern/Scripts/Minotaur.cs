3d6f414c8bb7b39547ca3d5ec6bdacb74f49f3e8
Kima09 <hkdavidl666@gmail.com>
2026-01-29 00:18:23 +0900

    (2026.1.20) 어댑터 패턴

---
+++ Program/Assets/Object Pool Pattern/Scripts/Minotaur.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Minotaur : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] Vector3 direction;
+    [SerializeField] Transform portalTransform;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        portalTransform = GameObject.Find("Portal").transform;
+        transform.LookAt(portalTransform);
+        direction = (portalTransform.position - transform.position).normalized;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction*speed*Time.deltaTime, Space.World);
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Portal"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
